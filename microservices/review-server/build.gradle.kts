7907677da0237738180875fb1ccccb6010a4de40
craftsangjae <craftsangjae@gmail.com>
2021-10-04 19:41:31 +0900

    build.gradle.kts에서 불필요한 의존성 제거

--- microservices/review-server/build.gradle.kts
+++ microservices/review-server/build.gradle.kts
@@ -6,16 +6,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    compileOnly("org.projectlombok:lombok")
-
-    runtimeOnly("com.h2database:h2")
-    runtimeOnly("mysql:mysql-connector-java")
-
-    annotationProcessor("org.projectlombok:lombok")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
